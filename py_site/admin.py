c29f79f3c51d2f64211eedc30c507d1f39395da8
AleksZ13ru <a.zaykin@list.ru>
2018-04-04 22:49:59 +0300

    Create models in py_site

--- py_site/admin.py
+++ py_site/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Machine, User, Note
+
 
 # Register your models here.
+
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ('location', 'title')
+    list_filter = ('location',)
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('machine', 'stop_equipment', 'date_start', 'text')
+
+
+admin.site.register(Machine, MachineAdmin)
+admin.site.register(User)
+admin.site.register(Note, NoteAdmin)
--- py_site/models.py
+++ py_site/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+# TODO: Hello
+
+
+class Machine(models.Model):
+    LOCATION = (
+        ('1', 'ПКС'),
+        ('7', 'ПСК')
+    )
+    location = models.CharField(choices=LOCATION, max_length=2)
+    title = models.CharField(max_length=20)
+
+
+class User(models.Model):
+    fio = models.CharField(max_length=40)
+
+
+class Note(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    date_start = models.DateField()
+    date_stop = models.DateField()
+    time_start = models.TimeField()
+    time_stop = models.TimeField()
+    stop_equipment = models.BooleanField()
+    user_start = models.ForeignKey(User, on_delete=models.PROTECT)
+    user_stop = models.ForeignKey(User, on_delete=models.PROTECT, related_name='+', default=None)
+    text = models.TextField(max_length=200)
