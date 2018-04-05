1801eeac8aa86ec2fa3186eabe32531913c4eeb6
AleksZ13ru <a.zaykin@list.ru>
2018-04-05 20:41:03 +0300

    fix pack.py file.

--- py_site/urls.py
+++ py_site/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.django, name='py_django'),
+    url(r'^notes$', views.notes, name='py_notes')
 ]
--- py_site/views.py
+++ py_site/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def django(request):
     return render(request, 'py_site/django.html')
+
+
+def notes(request):
+    return render(request, 'py_site/notes.html')
