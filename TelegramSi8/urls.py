17b9c80ced3e2c78e75c6dcfe57ba586f005d4c0
AleksZ13ru <a.zaykin@list.ru>
2018-04-20 01:51:07 +0300

    url add home page

--- TelegramSi8/urls.py
+++ TelegramSi8/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^viber/', include('py_viber.urls', namespace='viber')),
     # url(r'^', include('py_viber.urls', namespace='viber')),
     # url(r'^', include('si8_parsing.urls')),
-    # url(r'^', include('py_site.urls'))
+    url(r'^', include('py_site.urls'))
 ]
